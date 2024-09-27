d45fc84a142ed22a8e10d8a09f1dc3ed450a4d0a
adrianaska0 <adrian.aska004@gmail.com>
2024-09-26 22:20:16 -0400

    added docstrings and newlines to tests

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,13 +2,18 @@
 from calculator.operations import add, multiply, subtract, divide
 
 def test_add():
+    '''Test addition function works'''
     assert add(3,3) == 6
 
 def test_subtract():
+    '''Test subtraction function works'''
     assert subtract(3,3) == 0
 
 def test_multiply():
+    '''Test multiplication function works'''
     assert multiply (3,3) == 9
 
 def test_divide():
+    '''Test division function works'''
     assert divide(3,3) == 1
+    
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,13 +2,18 @@
 from calculator import Calculator
 
 def test_addition():
+    '''Test add function works'''
     assert Calculator.add(3,3) == 6
 
 def test_subtraction():
+    '''Test sub function works'''
     assert Calculator.sub(3,3) == 0
 
 def test_multiply():
+    '''Test mult function works'''
     assert Calculator.mult(3,3) == 9
 
 def test_divide():
+    '''Test div function works'''
     assert Calculator.div(3,3) == 1
+    
