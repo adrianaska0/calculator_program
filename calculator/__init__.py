e9a34320d617d926f88b5d508568590d8544cc94
adrianaska0 <adrian.aska004@gmail.com>
2024-09-26 23:16:27 -0400

    updated Calculator class

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,20 +1,28 @@
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
+from calculator.calculations import Calculations
+from decimal import Decimal
+from typing import Callable
 
 class Calculator:
 	@staticmethod
-	def add(a,b):
-		calculation = Calculation(a, b, add)
-		return calculation.get_res()
+	def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
+		c = Calculation.create(a, b, operation) #create object
+		Calculations.add_calculation(c) #add to history
+		return c.perform()
+
 	@staticmethod
-	def sub(a,b):
-		calculation = Calculation(a, b, subtract)
-		return calculation.get_res()
+	def add(a: Decimal, b: Decimal) -> Decimal:
+		return Calculator._perform_operation(a, b, add)
+
 	@staticmethod
-	def mult(a,b):
-		calculation = Calculation(a, b, multiply)
-		return calculation.get_res()
+	def sub(a: Decimal, b: Decimal) -> Decimal:
+		return Calculator._perform_operation(a, b, subtract)
+
 	@staticmethod
-	def div(a,b):
-		calculation = Calculation(a, b, divide)
-		return calculation.get_res()
+	def mult(a: Decimal, b: Decimal) -> Decimal:
+		return Calculator._perform_operation(a, b, multiply)
+
+	@staticmethod
+	def div(a: Decimal, b: Decimal) -> Decimal:
+		return Calculator._perform_operation(a, b, divide)
