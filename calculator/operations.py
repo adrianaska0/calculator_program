0e29ec7193e7fcb3e90b1b5bc4ad1445ea97db35
adrianaska0 <adrian.aska004@gmail.com>
2024-09-26 22:48:57 -0400

    updated ops to include type hints

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,11 +1,15 @@
-def add(a,b):
-	return a + b
+from decimal import decimal
 
-def subtract(a,b):
-	return a - b
+def add(a: Decimal, b: Decimal) -> Decimal:
+    return a + b
 
-def multiply(a,b):
+def subtract(a: Decimal, b:Decimal) -> Decimal:
+    return a - b
+
+def multiply(a: Decimal, b:Decimal) -> Decimal:
     return a * b
 
-def divide(a,b):
+def divide(a: Decimal, b:Decimal) -> Decimal:
+    if b == 0:
+        raise ValueError("Cannot divde by zero")
     return a / b
