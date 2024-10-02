fce56da274cf915e0860c485debd898cbd6c7ef9
adrianaska0 <adrian.aska004@gmail.com>
2024-10-02 19:28:08 -0400

    faker implementation with pytest test generation

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,26 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2)) if i % 4 != 3 else Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
+
+        if operation_func == divide:
+            b = Decimal('1') if b == Decimal('0') else b
+
+        try:
+            if operation_func == divide and b == Decimal('0'):
+                expected = "ZeroDivisionError"
+            else:
+                expected = operation_func(a, b)
+        except ZeroDivisionError:
+            expected = "ZeroDivisionError"
+
+        yield a, b, operation_name, operation_fun, expected
+
+def pytest_addoption(parser):
+    parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
+
+def pytest_generate_tests(metafunc):
+    if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
+        num_recs = metafunc.config.getoption("num_records")
+        params = list(generate_test_data(num_recs))
+        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in params]
+        metafunc.parametrize("a, b, operation, expected" modified_parameters)
