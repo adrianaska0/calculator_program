4db580752798fef2d4f59126df1be1ef2d2e3273
adrianaska0 <adrian.aska004@gmail.com>
2024-10-02 23:03:21 -0400

    added main.py for taking user input from cmd line

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from calculator import Calculator
+from decimal import Decimal, InvalidOperation
+
+def calculate_and_log(a, b, op_name):
+    op_mappings = {
+        'add' = Calculator.add,
+        'subtract' = Calculator.subtract,
+        'multiply' = Calculator.multiply,
+        'divide' = Calculator.divide
+    }
+
+    try:
+        a_decimal, b_decimal = map(Decimal, [a,b])
+        result = op_mappings.get(op_name)
+        if result:
+            print(f"The result of {a} {op_name} {b} is equal to {result(a_decimal, b_decimal)}")
+        else:
+            print(f"Unknown operation: {op_name}")
+    except InvalidOperation:
+        print(f"Invalid operand input: {a} or {b} is not a valid number.")
+    except ZeroDivisionError:
+        print("Error: Division by zero.")
+    except Exception as e:
+        print(f"An error occured: {e}")
+
+def main():
+    if len(sys.arg) != 4:
+        print("Usage: python calculator_main.py <number1> <number2> <operation>")
+        sys.exit(1)
+
+        _, a, b, operation = sys.argv
+        calculate_and_log(a, b, operation)
+
+        if __name__ == '__main__':
+            main()
